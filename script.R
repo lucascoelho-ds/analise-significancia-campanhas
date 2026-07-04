057032f82b0a73f72cd598fe09cb7659b9fd8730
Lucas Coelho <lucasbarbosacoelho@gmail.com>
2026-07-04 18:50:03 -0300

    Add script

--- script.R
+++ script.R
@@ -1,6 +1,12 @@
 # Dados
+conv_A <- 200
+total_A <- 1000
+conv_B <- 250
+total_B <- 1000
 
 # Proporções 
+prop_A <- conv_A/total_A
+prop_B <- conv_B/total_B
 
 # Teste de Proporções
-
+print(prop.test(c(conv_A, conv_B), c(total_A, total_B)))
